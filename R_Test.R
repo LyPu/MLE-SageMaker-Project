03048f08fc85375a53a7ed77ae659428cab865ce
Leiying Pu <leiyingpu@Leiyings-MacBook-Pro.local>
2019-08-21 19:07:55 -0700

    this is a practice

--- R_Test.R
+++
@@ -1,5 +0,0 @@
-#Calculate Cumulative Return
-set.seed(1) 
-returns <- runif(1000, 0.95,1.055) #Extremely simple return generation with a slight drift. 
-plot(cumprod(returns), type = "l")
-lines(cumsum(returns-1)+1, col = "blue")
